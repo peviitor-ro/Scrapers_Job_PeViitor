c5ad1532737936a513ca661a9ae24636d3b023a0
Stefania Vlad <vladstefania98@gmail.com>
2023-10-05 23:37:23 +0300

    updated EEUsoftware scraper

--- sites/eeusoftware_scraper.py
+++ sites/eeusoftware_scraper.py
@@ -3,6 +3,7 @@
 #
 # Scraper for EEU Software Company
 # Link to company career page -> https://www.eeusoft.ro/ro/category/cariere
+# https://careers.smartrecruiters.com/EEUSoftware
 #
 #
 #
@@ -19,15 +20,15 @@ def collect_data_from_eeusoftware():
     ... this function collects all data and returns a list with available jobs
     '''
 
-    response = requests.get(url='https://www.eeusoft.ro/ro/category/cariere', headers=DEFAULT_HEADERS)
+    response = requests.get(url='https://careers.smartrecruiters.com/EEUSoftware', headers=DEFAULT_HEADERS)
     soup = BeautifulSoup(response.text, 'lxml')
 
-    soup_data = soup.find_all('div', class_='card eeu-jobs-card my-2 mx-3 animated fadeInUp')
+    soup_data = soup.find_all('li', class_='opening-job job column wide-7of16 medium-1of2')
 
     lst_with_data = []
     for sd in soup_data:
-        title = sd.find('h6', class_='eeu-h6 pl-3 pt-2').text
-        link = sd.find('a', class_='btn eeu-jobs-btn2 text-center text-nowrap mt-1')['href']
+        title = sd.find('h4', class_='details-title job-title link--block-target').text
+        link = sd.find('a', class_='link--block details')['href']
 
         lst_with_data.append({
             "id": str(uuid.uuid4()),
@@ -35,7 +36,7 @@ def collect_data_from_eeusoftware():
             "job_link": link,
             "company": "EEUSoftware",
             "country": "Romania",
-            "city": "Romania"
+            "city": "Bucuresti"
         })
 
     return lst_with_data
