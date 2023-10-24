b8467ef59bae9fb4207c567f90c7c7d31f0b8074
Stefania Vlad <vladstefania98@gmail.com>
2023-10-24 21:21:22 +0300

    updates

--- sites/interratravel_scraper.py
+++ sites/interratravel_scraper.py
@@ -13,7 +13,7 @@ import uuid
 
 def collect_data_from_interra():
     '''
-    ... this function will collect all data and will return a list with available jobs
+    ... this function will collect all data and will return a list with jobs
     '''
     response = requests.get(url='https://www.interra.ro/angajari/', headers=DEFAULT_HEADERS)
     soup = BeautifulSoup(response.text, 'lxml')
